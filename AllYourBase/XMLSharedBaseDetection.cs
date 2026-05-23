73e437de56549fdecb96c45a805446b686a4b2ca
Halicade <qstr23@hotmail.com>
2026-05-23 18:51:36 -0400

    Update for 1.6 Now Verifies all Name attributes and defName

--- AllYourBase/XMLSharedBaseDetection.cs
+++ AllYourBase/XMLSharedBaseDetection.cs
@@ -13,89 +13,53 @@ namespace AllYourBase
     internal static class XmlSharedBaseDetection
     {
         /// <summary>
-        /// INTENT: Decrease compatibility issues caused by modders overwriting (abstract) bases, by making it easy to detect the existence of these bad practices.
+        /// INTENT: Decrease compatibility issues caused by modders overwriting (abstract) bases or defNames, by making it easy to detect the existence of these bad practices.
         /// 
-        /// Run once on startup. Creates list of all (abstract) bases in vanilla, then compares with bases in mods. Warns with mod name, base and exact filename for any matches found. 
+        /// Run once on startup. Continually adds all Name attributes and defNames into a list to compare against other entries.
+        /// The only limit this has, is it does not account for defNames used in abstract Defs
         /// </summary>
-        static XmlSharedBaseDetection()
-        {
+        static XmlSharedBaseDetection() {
             //since some people have such wonderfully organised mod lists they hit the 1k limit on error logging:
             Log.ResetMessageCount();
 
             //get all bases in vanilla.
-            List<string> vanillaXmlAttributes = new List<string>();
+            HashSet<string> listXMLAttributes = new HashSet<string>();
+            HashSet<string> listDefNames = new HashSet<string>();
 
-            foreach (ModContentPack mod in LoadedModManager.RunningMods.Where(mod => mod.IsCoreMod))
-            {
-                foreach (LoadableXmlAsset asset in DirectXmlLoader.XmlAssetsInModFolder(mod, "Defs/"))
-                {
+            foreach (ModContentPack mod in LoadedModManager.RunningMods) {
+                foreach (LoadableXmlAsset asset in DirectXmlLoader.XmlAssetsInModFolder(mod, "Defs/")) {
                     if (asset.xmlDoc?.DocumentElement == null) continue;
                     XmlNodeList childNodes = asset.xmlDoc.DocumentElement.ChildNodes;
                     {
-                        for (int i = 0; i < childNodes.Count; i++)
-                        {
+                        for (int i = 0; i < childNodes.Count; i++) {
                             if (childNodes[i].NodeType != XmlNodeType.Element) continue;
-                            if (childNodes[i]?.Attributes?["Name"] != null)
-                            {
-                                vanillaXmlAttributes.Add(childNodes[i].Attributes.GetNamedItem("Name").Value);
+                            string nameAttribute = childNodes[i]?.Attributes?["Name"]?.Value;
+                            if (nameAttribute != null) {
+                                if (!listXMLAttributes.Add(nameAttribute)) {
+                                    Log.Error("[" + asset.mod.Name + "]" +
+                                              " can cause inheritance errors by overwriting attribute " +
+                                              nameAttribute + " in file " + asset.FullFilePath);
+                                }
+                            }
+
+                            if (childNodes[i]?.Attributes?["Abstract"]?.Value != null) {
+                                continue;
+                            }
+
+                            string itemDefName = childNodes[i]?.SelectSingleNode("defName")?.InnerText;
+                            if (itemDefName != null) {
+                                if (!listDefNames.Add(childNodes[i].Name + "_" + itemDefName)) {
+                                    Log.Error("[" + asset.mod.Name + "]" +
+                                              " can cause compatibility errors by overwriting " +
+                                              childNodes[i].Name + " " +
+                                              itemDefName + " in file " + asset.FullFilePath);
+                                }
                             }
                         }
                     }
                 }
             }
-
-            if (Prefs.LogVerbose)
-                Log.Error("Verbose mode detected. AllYourBase will attempt to fix compatibility errors. If problem persists or gets worse, Verify File Integrity or redownload affected mods.");
-
-            //get all bases in mods and compare them to the vanilla list.
-            foreach (ModContentPack mod in LoadedModManager.RunningMods.Where(mod => !mod.IsCoreMod))
-            {
-                foreach (LoadableXmlAsset asset in DirectXmlLoader.XmlAssetsInModFolder(mod, "Defs/"))
-                {
-                    bool dirty = false;
-                    if (asset.xmlDoc?.DocumentElement == null) continue;
-                    XmlNodeList childNodes = asset.xmlDoc.DocumentElement.ChildNodes;
-                    {
-                        for (int i = childNodes.Count -1 ; i >= 0; i--)
-                        {
-                            if (childNodes[i].NodeType != XmlNodeType.Element) continue;
-
-                            if (childNodes[i]?.Attributes?["Name"] != null &&
-                                vanillaXmlAttributes.Contains(childNodes[i].Attributes.GetNamedItem("Name").Value))
-                            {
-                                Log.Warning("[" + asset.mod.Name + "]" + " causes compatibility errors by overwriting " +
-                                            childNodes[i].Attributes.GetNamedItem("Name").Value + " in file " + asset.FullFilePath);
-
-                                if (Prefs.LogVerbose)
-                                {
-                                    Log.Message($"Attempting fix for {childNodes[i].Attributes.GetNamedItem("Name").Value}");
-                                    dirty = true;
-                                    asset.xmlDoc.DocumentElement.RemoveChild(childNodes[i]);
-                                }
-                                else
-                                {
-                                    Log.Message("If you enable Verbose Logging, AllYourBase will attempt to fix compatibility errors.");
-                                }
-                            }
-                        }
-                    }
-                    if (dirty)
-                    {
-                        asset.xmlDoc.Save(asset.FullFilePath);
-                    }
-                }
-            }
-            if (Prefs.LogVerbose)
-            {
-                Log.Message("AllYourBase has completed its attempt to fix compatibility errors, and will now turn off verbose logging.");
-                Prefs.LogVerbose = false;
-            }
-
-            foreach (ChemicalDef item in DefDatabase<ChemicalDef>.AllDefsListForReading)
-            {
-                if (item.addictionHediff?.hediffClass == null)
-                    Log.Error($"{item.defName} from mod {item.modContentPack.Name} has no addictionHediff (or missing hediffClass). This will break raids and worldgen. Misconfigured XML or parent.");
-            }
+            
         }
     }
 }
